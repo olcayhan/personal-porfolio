128f4fa718c69294b801e66085852595eea18d70
olcayhank <kolcayhan1@gmail.com>
2023-08-22 10:55:29 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Main from "@/components/Main";
-import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 
 export default function Home() {
 
   return (
     <>
-      <Navbar />
       <Main />
       <About/>
       <Projects />
